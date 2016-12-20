8d8228a24e8822947690cd3f0b5dea80a63978ff
Colson Hoxie <colsonhoxie@Colsons-MacBook-Pro.local>
2016-12-20 13:33:18 -0800

    initial commit

---
+++ Num2ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  Num2ViewController.swift
+//  learnCalc
+//
+//  Created by Colson Hoxie on 12/19/16.
+//  Copyright © 2016 Colson Hoxie. All rights reserved.
+//
+
+import UIKit
+var solutionX: UInt32 = 0
+var solutionY: UInt32 = 0
+
+
+class Num2ViewController: UIViewController {
+
+    //MARK: Properties
+    @IBOutlet weak var functionOne: UILabel!
+    @IBOutlet weak var functionTwo: UILabel!
+    @IBOutlet weak var solution: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        functionOne.text = "\(String(x1))x +  \(String(y1))y = \(String(z1))"
+        functionTwo.text = "\(String(x2))x +  \(String(y2))y = \(String(z2))"
+        solution.text = "Solution will be output here"
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    //MARK: Actions
+    
+    //Function to solve the system of equations
+    @IBAction func solver(_ sender: UIButton) {
+        var a: Double = 0
+        var b: Double = 0
+        
+        //If the if statement is false, we have no solution. However, if it is true, we can solve for X and Y using a basic matrix method
+    
+        if x1*y2 - x2*y1 != 0 {
+            let inverseCo = 1/(x1*y2 - x2*y1)
+            
+            a = inverseCo*(z1*y2-y1*z2)
+            b = inverseCo*(x1*z2-x2*z1)
+            solution.text = "x = \(String(a)) and y = \(String(b))"
+        }
+        else {
+            solution.text = "There is no solution"
+        }
+    }
+
+}
--- learnCalc/ViewController.swift
+++ learnCalc/ViewController.swift
@@ -8,18 +8,109 @@
 
 import UIKit
 
+//All variables will be doubles to have "exact" solutions
+var x1: Double = 0
+var x2: Double = 0
+var y1: Double = 0
+var y2: Double = 0
+var z1: Double = 0
+var z2: Double = 0
+
+
 class ViewController: UIViewController {
 
+    //MARK: Properties
+    @IBOutlet weak var funcGenerated: UILabel!
+    @IBOutlet weak var funcGenerated2: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        funcGenerated.text = "Function 1"
+        funcGenerated2.text = "Function 2"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: Actions
+    
+    //Generates 2 random equations that we want to solve
+    @IBAction func generateFunction(_ sender: UIButton) {
+        
+        self.randomize()
+        print(x1)
+        print(x2)
+        print(y1)
+        print(y2)
+        
+        funcGenerated.text = "\(String(x1))x +  \(String(y1))y = \(String(z1))"
+        
+        funcGenerated2.text = "\(String(x2))x +  \(String(y2))y = \(String(z2))"
+    }
 
+    //This function creates random variables and will call the isNegative function to create potential negative values
+    
+    func randomize() {
+        //x1 variable
+        if isNegative() == true {
+            x1 = -Double(arc4random_uniform(19)) + 1
+        }
+        if isNegative() == false {
+            x1 = Double(arc4random_uniform(19)) + 1
+        }
+        
+        //x2 variable
+        if isNegative() == true {
+            x2 = -Double(arc4random_uniform(19)) + 1
+        }
+        if isNegative() == false {
+            x2 = Double(arc4random_uniform(19)) + 1
+        }
+        
+        //y1 variable
+        if isNegative() == true {
+            y1 = -Double(arc4random_uniform(19)) + 1
+        }
+        if isNegative() == false {
+            y1 = Double(arc4random_uniform(19)) + 1
+        }
+        
+        //y2 variable
+        if isNegative() == true {
+            y2 = -Double(arc4random_uniform(19)) + 1
+        }
+        if isNegative() == false {
+            y2 = Double(arc4random_uniform(19)) + 1
+        }
+        
+        //z1 variable
+        if isNegative() == true {
+            z1 = -Double(arc4random_uniform(20))
+        }
+        if isNegative() == false {
+            z1 = Double(arc4random_uniform(20))
+        }
+        
+        //z2 variable
+        if isNegative() == true {
+            z2 = -Double(arc4random_uniform(20))
+        }
+        if isNegative() == false {
+            z2 = Double(arc4random_uniform(20))
+        }
+    }
+    
+    //Called to determine if a variable will be negative or not
+    func isNegative() -> Bool {
+        if arc4random_uniform(2) == 1 {
+            return true
+        }
+        else {
+            return false
+        }
+    }
 
 }
-
